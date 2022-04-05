916ca760699751f87b681b41a1833ad0582f6b41
sherwin-kwan <sherwin.kwan92@gmail.com>
2022-04-04 22:39:15 -0700

    add initial CRUD routes, homepage, and admin articles list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,27 +3,7 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def create
-    @article = Article.new(permitted_params)
-    if @article.save
-      redirect_to articles_path
-    else
-      puts "Something went wrong"
-      # TODO: Add a flash to handle errors
-    end
-  end
-
-  def new
-    @article = Article.new
-  end
-
   def show
     @article = Article.find(params[:id])
-  end
-
-  private
-
-  def permitted_params
-    params.require(:article).permit([:title, :body])
   end
 end
