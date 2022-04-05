3d713d3462ec120ae4eaf4c947799a2c6c5ae6fc
sherwin-kwan <sherwin.kwan92@gmail.com>
2022-04-04 22:39:15 -0700

    add initial CRUD routes, homepage, and admin articles list

---
+++ app/controllers/admin/articles_controller.rb
@@ -0,0 +1,45 @@
+class Admin::ArticlesController < ApplicationController
+  def index
+    @articles = Article.all.order("created_at DESC")
+  end
+
+  def create
+    @article = Article.new(permitted_params)
+    if @article.save
+      redirect_to admin_articles_path
+    else
+      puts "Something went wrong"
+      # TODO: Add a flash to handle errors
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      redirect_to admin_articles_path
+      # TODO: Notify user that deleted successfully
+    else
+      puts "Something went wrong"
+      # TODO: Add a flash
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(permitted_params)
+    redirect_to 
+  end
+
+  private
+
+  def permitted_params
+    params.require(:article).permit([:title, :body])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
-  resources :articles
+  root "articles#index"
+  resources :articles, only: %i(index show)
+  namespace :admin do
+    resources :articles
+  end
 end
