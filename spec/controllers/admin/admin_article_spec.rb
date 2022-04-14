a145fd73df25015f95c01e1ac54597a992415ca1
sherwin-kwan <sherwin.kwan92@gmail.com>
2022-04-13 21:25:59 -0700

    test doing request specs instead of controller specs

--- spec/controllers/admin/admin_article_spec.rb
+++ spec/controllers/admin/admin_article_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Admin::ArticlesController do
+RSpec.describe "Admin Articles cotroller", type: :request do
 
   before(:each) do
     a = Article.create({title: "Sample Title", body: "This is a Body"})
@@ -9,7 +9,7 @@ RSpec.describe Admin::ArticlesController do
 
   describe "index" do
     it "renders the index template with the right status code" do
-      get :index
+      get "/admin/articles"
       expect(response).to render_template("index")
       expect(response.status).to eq(200)
     end
@@ -17,7 +17,7 @@ RSpec.describe Admin::ArticlesController do
 
   describe "new" do
     it "renders the new template with the right status code" do
-      get :new
+      get "/admin/articles/new"
       expect(response).to render_template("new")
       expect(response.status).to eq(200)
     end
@@ -25,18 +25,18 @@ RSpec.describe Admin::ArticlesController do
 
   describe "create" do
     it "should fail if you try to create articles without a title" do
-      post :create, params: {article: {title: ""}}
+      post "/admin/articles", params: {article: {title: ""}}
       expect(Article.count).to eq(1)
       expect(response.status).to eq(400)
     end
 
     it "should show a relevant error message" do 
-      post :create, params: {article: {title: ""}}
+      post "/admin/articles", params: {article: {title: ""}}
       expect(flash[:errors]).to include("could not be created")
     end
 
     it "should create a new article if everything's filled out" do
-      post :create, params: {article: {title: "New Title", body: "My Body"}}
+      post "/admin/articles", params: {article: {title: "New Title", body: "My Body"}}
       expect(response).to redirect_to(admin_articles_path)
       expect(Article.find_by(title: "New Title")).to be_truthy
     end
@@ -44,7 +44,7 @@ RSpec.describe Admin::ArticlesController do
 
   describe "edit" do
     it "renders the edit template with the right status code" do
-      get :edit, params: {id: @id}
+      get "/admin/articles/#{@id}/edit"
       expect(response).to render_template("edit")
       expect(response.status).to eq(200)
     end
@@ -52,17 +52,17 @@ RSpec.describe Admin::ArticlesController do
 
   describe "update" do
     it "should fail if you try to update articles to drop the title" do
-      put :update, params: {id: @id, article: {title: ""}}
+      put "/admin/articles/#{@id}", params: {id: @id, article: {title: ""}}
       expect(response.status).to eq(400)
     end
 
     it "should show a relevant error message" do 
-      put :update, params: {id: @id, article: {title: ""}}
+      put "/admin/articles/#{@id}", params: {id: @id, article: {title: ""}}
       expect(flash[:errors]).to include("could not be updated")
     end
 
     it "should update the article if everything's filled out" do
-      put :update, params: {id: @id, article: {title: "You Didn't Expect This, Did You?", body: "My Body"}}
+      put "/admin/articles/#{@id}", params: {id: @id, article: {title: "You Didn't Expect This, Did You?", body: "My Body"}}
       expect(response).to redirect_to(admin_articles_path)
       expect(Article.find(@id).title).to eq("You Didn't Expect This, Did You?")
     end
@@ -70,7 +70,7 @@ RSpec.describe Admin::ArticlesController do
 
   describe "destroy" do
     it "should destroy an article" do
-      delete :destroy, params: {id: @id}
+      delete "/admin/articles/#{@id}", params: {id: @id}
       expect{ Article.find(@id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
