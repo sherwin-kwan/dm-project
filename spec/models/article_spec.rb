72f6b96ecc2a0af9054dac2a0df93efed471aa3c
sherwin-kwan <sherwin.kwan92@gmail.com>
2022-04-22 15:55:37 -0700

    updating model spec with factorybot

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+
+  let(:a) { FactoryBot.build(:article, title: "", body: "This is a body") }
+  let(:b) { FactoryBot.build(:article, title: "This is a title", body: "") }
+  let(:c) { FactoryBot.build(:article, title: "This is a title", body: "And a body!") }
+
+
   it "shouldn't let you save an article with no title" do
-    a = Article.new({title: "", body: "This is a body"})
     expect(a.save).to be(false)
     expect(Article.count).to eq(0)
   end
 
   it "should NOT let us create an article with no body" do
-    a = Article.new({title: "This is a title", body: ""})
-    expect(a.save).to be(false)
+    expect(b.save).to be(false)
     expect(Article.count).to eq(0)
   end
 
   it "should ALLOW us to create an article with a title and a body" do
-    a = Article.new({ title: "My title", body: "The body of the article" })
-    expect(a.save).to be(true)
+    expect(c.save).to be(true)
     expect(Article.count).to eq(1)
   end
 end
