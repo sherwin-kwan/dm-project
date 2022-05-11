27af71d08ac57445222b3d407476c248c42837a6
sherwin-kwan <sherwin.kwan92@gmail.com>
2022-05-10 18:14:30 -0700

    add ID to the article route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,34 +1,15 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
-  validates :slug, uniqueness: true
 
-  before_save :set_slug
+  before_save :set_slug, :if => Proc.new{ self.title_changed? }
 
   def to_param
-    self.slug || self.id
+    "#{self.id}-#{self.slug}"
   end
 
   def set_slug
-    base_slug = self.title.parameterize(separator: "_")
-    counter = 1
-    current_slug = base_slug
-    while self.class.find_by(slug: current_slug)
-      current_slug = "#{base_slug}_#{counter.to_s}"
-      counter += 1
-    end
-    self.slug = current_slug
+    self.slug = self.title.parameterize(separator: "-")
   end
 
-  class << self
-    # Where "arg" could be either a string slug or an integer ID
-    def find_by_slug_or_id(arg)
-      if arg.to_i > 0
-        @article = Article.find(arg)
-      else
-        @article = Article.where(slug: arg).first
-      end
-    end
-
-  end
 end
