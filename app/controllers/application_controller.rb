3e265ffea0f2ca90cfb29ffe22c569ebc8172221
sherwin-kwan <sherwin.kwan92@gmail.com>
2022-07-04 21:34:26 -0700

    set up pagination on both front-end and back-end using a reusable module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :get_path
+
+  def get_path(path_prefix, options = {})
+    return Rails.application.routes.url_helpers.send("#{path_prefix}_path", options)
+  end
 end
