628a840aa1e84b3010af12d71fc94e7bf591a280
sherwin-kwan <sherwin.kwan92@gmail.com>
2022-07-13 09:14:46 -0700

    add tinymce editor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,21 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "pagination"
+import "tinymce"
+
+
+window.addEventListener('turbo:load', async (event) => {
+  tinymce.init({
+    selector: '.tinymce',
+    height: 500,
+    menubar: false,
+    plugins: [
+      'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+      'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+      'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+    ],
+    toolbar: 'undo redo | casechange blocks | bold italic backcolor | ' +
+      'alignleft aligncenter alignright alignjustify | ' +
+      'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+  });
+})
