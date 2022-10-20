9edc017862566c5ef2159804a7b220c46508f33a
sherwin-kwan <sherwin.kwan92@gmail.com>
2022-10-20 16:39:35 -0700

    set up admin controller; only authenticated users can do create/edit/delete

---
+++ spec/controllers/authentication_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+Rspec.describe "Authentication", type: :request do
+  it "user can log in" do
+
+  end
+
+  it "once logged in, user can log out" do
+
+  end
+
+  it "users can't visit an admin page until they're logged in" do
+
+  end
+
+  it "when not logged in, user sees 'Log In' and 'Sign Up' on the navbar" do
+
+  end
+
+  it "once logged in, user sees 'Log Out' on the navbar" do
+    
+  end
+end
